027a9116392d95a28ef98dd86386d71697a50fa1
Thiago Klein de Angelis <thiagokda1@gmail.com>
2016-04-10 22:04:24 -0300

    Organização

---
+++ T1/1.1/src/Filosofo.java
@@ -0,0 +1,29 @@
+
+public class Filosofo {
+
+	private static int numFilosofos = 0;
+	private int myNum;
+	
+	Filosofo() {
+		this.myNum = numFilosofos;
+		numFilosofos++;	
+	}
+	void think() {
+		
+	}
+	void take_fork() {
+		
+	}
+	void eat() {
+		
+	}
+	void put_fork() {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
